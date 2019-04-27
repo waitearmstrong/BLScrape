f6bfba8756ffc06e12946fb8b097a5ed37a5ae09
Waite Armstrong <wha0001@mix.wvu.edu>
2019-04-27 18:41:20 -0400

    Add files via upload
    Finally figured out a way to accurately scrape the state name every time. Got it to create a text file with proper naming. Now trying to get it to write comma separated values to the text file and it is not recognizing the file as an object.

--- BLScrape.py
+++ BLScrape.py
@@ -5,14 +5,13 @@ from bs4 import BeautifulSoup
 #creating a properly named file to write data to
 today = date.today()
 name = today.strftime("%Y%m%d") + ".txt"
-f = open(str(name),"w+")
+file = open(str(name),"w+")
 #base url 
 url = 'https://www.bls.gov/eag/home.htm'
 response = requests.get(url)
 soup = BeautifulSoup(response.content,'html.parser')
 #gather all the links from the sidebar, then modify the base url into an array of usable state urls
 listPre = soup.find_all('div', class_='secondary-nav')[-1]
-#listPost = listPre.find_all('a')
 links = []
 for link in listPre.find_all('a'):
     links.append("https://www.bls.gov" + link.get('href'))
@@ -21,13 +20,19 @@ for link in links:
     soupData = BeautifulSoup(requests.get(link).content,'html.parser')
     try:
         dataTable = soupData.find('tbody')
-        stateName = ""
+        #Scrape the state name
+        stateName = soupData.find("div", { "id" : "programs-main-content" }).find('h2').text
+        #Scrape the Labor Force Rate and strip the unneeded text
         rOne = dataTable.find_all('tr')[1]
         laborForce = rOne.find_all('span')[-2].text.strip("(p)")
+        #Scrape the Unemployment rate and strip the unneeded text
         rTwo = dataTable.find_all('tr')[4]
         unemRate = rTwo.find_all('span')[-2].text.strip("(p)")
-        print("Current Civilian Labor Force rate for " + stateName + " is " + laborForce)
+        #Ouput both values to the console as neatly formatted strings.
         print("Current Unemployment rate for " + stateName + " is " + unemRate)
+        print("Current Civilian Labor Force rate for " + stateName + " is " + laborForce)
+        file.write(stateName + "," + str(unemRate) + "," + today.strftime("%Y%m%d")
+        file.write(stateName + "," + str(laborForce) + "," +today.strftime("%Y%m%d")
     except:
         print("Value not valid")
         pass
