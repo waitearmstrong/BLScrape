7df9806a92f0b848b74eb5407c5e4f3dbb8b911c
Waite Armstrong <wha0001@mix.wvu.edu>
2019-04-28 12:41:46 -0400

    Finished Assignment
    Finished assignment.
    
    Program would not write to a file because I was missing a parentheses on the other end of the string.
    Added functionality for it to scrape US employment data in addition to individual state data.

--- BLScrape.py
+++ BLScrape.py
@@ -1,3 +1,7 @@
+#Name:       Waite Armstrong
+#ID:         800180062
+#Assignment: Final Assignment
+
 #importing necessasary libraries
 from datetime import date
 import requests
@@ -5,7 +9,7 @@ from bs4 import BeautifulSoup
 #creating a properly named file to write data to
 today = date.today()
 name = today.strftime("%Y%m%d") + ".txt"
-file = open(str(name),"w+")
+file = open(str(name),"w")
 #base url 
 url = 'https://www.bls.gov/eag/home.htm'
 response = requests.get(url)
@@ -24,16 +28,26 @@ for link in links:
         stateName = soupData.find("div", { "id" : "programs-main-content" }).find('h2').text
         #Scrape the Labor Force Rate and strip the unneeded text
         rOne = dataTable.find_all('tr')[1]
-        laborForce = rOne.find_all('span')[-2].text.strip("(p)")
+        laborForce = rOne.find_all('span')[-2].text.strip("(p)").strip(',')
         #Scrape the Unemployment rate and strip the unneeded text
         rTwo = dataTable.find_all('tr')[4]
-        unemRate = rTwo.find_all('span')[-2].text.strip("(p)")
+        unemRate = rTwo.find_all('span')[-2].text.strip("(p)").strip(',')
         #Ouput both values to the console as neatly formatted strings.
         print("Current Unemployment rate for " + stateName + " is " + unemRate)
         print("Current Civilian Labor Force rate for " + stateName + " is " + laborForce)
-        file.write(stateName + "," + str(unemRate) + "," + today.strftime("%Y%m%d")
-        file.write(stateName + "," + str(laborForce) + "," +today.strftime("%Y%m%d")
+        file.write(str(stateName) + "," + str(unemRate) + "," + today.strftime("%Y%m%d") + '\n')
+        file.write(str(stateName) + "," + str(laborForce) + "," +today.strftime("%Y%m%d") + '\n')
     except:
         print("Value not valid")
         pass
-f.close()
+print("--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+")
+#Scrape the Unemployment rate and strip the unneeded characters
+#The US employment data did not provide a labor force participation rate
+#Averaging the data from all the states would produce innaccurate data because
+#each state has different populations
+usLink = "https://www.bls.gov/eag/eag.us.htm"
+usSoupData = BeautifulSoup(requests.get(usLink).content,'html.parser')
+usDataTable = usSoupData.find('tbody').find_all('tr')[0]
+usUnemRate = usDataTable.find_all('span')[-2].text.strip("(p)").strip(',')
+print("Current Unemployment rate for the United States is " + usUnemRate)
+file.close()
